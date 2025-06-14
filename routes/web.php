378c8e5bb31b897dab34e82018f9e44b9dbabc6f
Developer2250 <superuser2312@gmail.com>
2025-06-14 10:11:35 +0530

    Adding Routes in welcome page

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+
 Route::resource('products', ProductController::class);
 
 use App\Http\Controllers\AuthorController;
+
 Route::resource('authors', AuthorController::class);
 
 use App\Http\Controllers\BookController;
+
 Route::resource('books', BookController::class);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
